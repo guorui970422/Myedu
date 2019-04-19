25e82c40755037399c621c92cf195017207fe0f0
郭瑞 <2257236939@qq.com>
2019-04-19 15:24:09 +0800

    list_type 删除

--- day02/list_type.py
+++ day02/list_type.py
@@ -7,6 +7,18 @@ def list_sel(a):
     # print(a[0:])
     # 去中间的值  x-1:y+1 (x表示从哪个值开始，y表示要取到的值)
     print(a[1:9])
+
+
+
+def list_del():
+    alist = ['快乐', 4396, '魔力瞎', '明凯', 'edg_win', '666', '777', '666', '777', '666', '777', '666', '777']
+    blist = ['c','b','d']
+    alist.pop(9)
+    blist.pop(1)
+    print(alist)
+    print(blist)
+
+
+
 if __name__ == '__main__':
-    alist = ['快乐',4396,'魔力瞎','明凯','edg_win','666','777','666','777','666','777','666','777']
-    list_sel(alist)
+    list_del()
