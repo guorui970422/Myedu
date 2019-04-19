9bbbfe6e633515a4c80c9f323ddadaf6549d9841
郭瑞 <2257236939@qq.com>
2019-04-19 18:00:06 +0800

    other_typ 布尔类型

--- day02/dict_typ.py
+++ day02/dict_typ.py
@@ -1,10 +1,33 @@
+import json
 adict = {"username":"yansl","password":"123456"}
-bdict = {5:"yansl","password":[2,5]}
-
-
+bdict = {'5':"yansl","password":[2,5]}
+adict1 = '{"username":"yansl","password":"123456"}'
 def dict_test():
     adict.pop('password')
     print(adict)
+def dict_add():
+    adict.update(bdict)
+    print(adict)
+    cdict = dict(adict,**bdict)
+    print(cdict)
+def dict_update():
+    adict["username"] = '快乐风男'
+    bdict['password'] = '战绩0-25'
+    adict.update(bdict)
+    print(adict)
+def ste2dict():
+    ster1 = json.dumps(adict)
+    print(ster1)
+    print(type(ster1))
+def dict_2():
+    print(type(adict1))
+    adict2 = json.loads(adict1)
+    print(adict2)
+    print(type(adict2))
 
 if __name__ == '__main__':
-    dict_test()
+    # dict_test()
+    # dict_add()
+    # ste2dict()
+    dict_2()
+
---
+++ day02/other_typ.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    abool = True
+    bbool = False
+    print(abool)
+    print(type(abool))
