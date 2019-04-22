50b4956a4223c2ae417ab859166f6b5eb26350e1
郭瑞 <2257236939@qq.com>
2019-04-22 16:53:35 +0800

    第三天课堂练习

--- day03/file_demo.py
+++ day03/file_demo.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     # test_io = open('test_text','w+')
     # test_io.write('我的男刀很强')
-    text_io = open('test_text','a+')
-    text_io.write('我的快乐风男也很强')
+    # text_io = open('test_text','a+')
+    # text_io.write('我的快乐风男也很强')
+    text_io = open('test_text','r')
--- day03/if_eles.py
+++ day03/if_eles.py
@@ -14,5 +14,9 @@ def if_else():
 
 
 if __name__ == '__main__':
-
-    if_else()
+    if __name__ == '__main__':
+        a = 0
+        # 语法: while 条件 : XX代码
+        while a < 5:
+            print(a)
+            a += 1
